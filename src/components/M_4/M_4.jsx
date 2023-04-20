df1a7f83572b33ca83d3452c84234ec6e9c27b5b
Oleksandr Kharutsa <al.haruca@gmail.com>
2023-04-20 21:32:37 +0200

    m_4 component is done

--- src/components/M_4/M_4.jsx
+++ src/components/M_4/M_4.jsx
@@ -2,6 +2,40 @@ import React from 'react';
 
 export const M_4 = () => {
   return (
-    <div>M_4</div>
+    <div className="m4">
+      <div className="m4__text">
+        Прохождение теста займет у вас не более
+        <span style={{color: "#FFC700"}}>
+          {" 12 минут"}
+        </span>
+        , а его результаты вы сможете
+        <span style={{color: "#FFC700"}}>
+          {" использовать всю жизнь."}
+        </span>
+      </div>
+
+      <div className="m4__wrapper">
+        Профессиональная интерпретация и детально
+        <span style={{fontWeight: "bold"}}>
+          {" проработанные рекомендации "}
+        </span>
+        позволят вам качественно
+        <span style={{fontWeight: "bold"}}>
+          {" изменить все аспекты своей жизни: "}
+        </span>
+        от финансового до любовного.
+      </div>
+
+      <button
+        type="button"
+        className="m4__button"
+      >
+        пройти тест
+      </button>
+
+      <p className="m4__copyright">
+        2019
+      </p>
+    </div>
   );
 };
